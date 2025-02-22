a249cdb45b95fce4e0ec393aacf5fa44e2c17ea6
DuxiiT <johony.mahfoud06@gmail.com>
2025-02-22 13:05:19 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,12 +1,9 @@
-// Add event listener to monitor the scroll position
 window.addEventListener("scroll", function () {
     const goToTopBtn = document.getElementById("goToTopBtn");
-    const aboutSection = document.getElementById("slide4"); // Assuming your "About" section has the ID "slide2"
+    const aboutSection = document.getElementById("slide4");
 
-    // Check the position of the "About" section
     const aboutTop = aboutSection.getBoundingClientRect().top;
 
-    // If the "About" section is within view, show the button
     if (aboutTop <= window.innerHeight && window.scrollY > 0) {
         goToTopBtn.classList.add("show");
     } else {
@@ -14,7 +11,6 @@ window.addEventListener("scroll", function () {
     }
 });
 
-// Scroll to top when the button is clicked
 document.getElementById("goToTopBtn").addEventListener("click", function () {
     window.scrollTo({ top: 0, behavior: "smooth" });
 });
